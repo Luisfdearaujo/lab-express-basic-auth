270dc77a1237ea8df2af11fa133b03502eff6655
Luisfdearaujo <90002192+Luisfdearaujo@users.noreply.github.com>
2021-11-18 08:14:03 +0000

    Lab-express

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
+const isLoggedIn = require("./../middleware/isLoggedIn");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+// GET /main
+router.get("/main", (req, res) => {
+  res.render("secret-view");
+});
+
+// GET /private
+router.get("/private", (req, res) => {
+  res.render("secret-page");
 });
 
 module.exports = router;
